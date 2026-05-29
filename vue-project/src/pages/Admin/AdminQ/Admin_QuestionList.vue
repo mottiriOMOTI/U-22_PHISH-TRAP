252860663961ac028c8328222a1d5eac130ad978
igada <iga.dai0226@gmail.com>
2026-05-29 12:17:16 +0900

    問題一覧途中

--- vue-project/src/pages/Admin/AdminQ/Admin_QuestionList.vue
+++ vue-project/src/pages/Admin/AdminQ/Admin_QuestionList.vue
@@ -1,10 +1,30 @@
 <template>
-
+    <v-card class="question-card ma-n6" variant="outlined" :title="questionListTitle" subtitle="いい">
+        <!-- ここにv-forで問題ごとにカードを入れる -->
+        <v-for></v-for>
+    </v-card>
 </template>
 
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed } from 'vue'
+import {
+    question_scenario,
+    scenarioSelectItems,
+} from '@/stores/admin_auestionList'
 
+const scenarioStore = question_scenario()
+
+const questionListTitle = computed(() => {
+    const selectedScenario = scenarioSelectItems.find(
+        (item) => item.value === scenarioStore.scenario,
+    )
+
+    return `${selectedScenario?.title ?? ''}の問題一覧`
+})
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.question-card {
+    border-color: rgba(var(--v-border-color), 0.12);
+}
+</style>
